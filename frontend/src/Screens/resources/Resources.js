a4c5374cefe9a8ed3435818d748a18c6157012bf
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-03-22 23:37:40 +0800

    Update Resources.js

--- frontend/src/Screens/resources/Resources.js
+++ frontend/src/Screens/resources/Resources.js
@@ -4,9 +4,16 @@ export default class Resources extends React.Component {
     render() {
         return (
             <div>
-                <p>Hello Resources!</p>
-                <h3>Support</h3>
-                <h3>Shop</h3>
+                <p>The community is SUPPORTIVE</p>
+                <section>
+                    <h3>COVID Information</h3>
+                </section>
+                <section>
+                    <h3>You can always get mental support!</h3>
+                </section>
+                <section>
+                    <h3>Emergency</h3>
+                </section>
             </div>        
         )
     }
