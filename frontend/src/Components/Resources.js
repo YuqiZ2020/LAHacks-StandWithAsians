18e4271664ddb331f11d13f68be50cbc3c597816
Yunqiu Han <yunqiu21@g.ucla.edu>
2021-03-22 17:08:06 +0800

    merge

--- frontend/src/Components/Resources.js
+++ frontend/src/Components/Resources.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default class Resources extends React.Component {
     render() {
         return (
-            <div>Hello Resources!</div>
-            <h3>Support</h3>
-            <h3>Shop</h3>
+            <div>
+                <p>Hello Resources!</p>
+                <h3>Support</h3>
+                <h3>Shop</h3>
+            </div>
+            
         )
     }
 }
