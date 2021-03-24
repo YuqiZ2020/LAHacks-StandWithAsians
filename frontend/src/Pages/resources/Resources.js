ec5949d56868b953ebdf3d4580641c140f22b44e
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-03-24 20:55:23 +0800

    Update Resources.js

--- frontend/src/Pages/resources/Resources.js
+++ frontend/src/Pages/resources/Resources.js
@@ -1,61 +1,30 @@
 import React from 'react';
-import {
-    Accordion,
-    AccordionItem,
-    AccordionItemButton,
-    AccordionItemHeading,
-    AccordionItemPanel,
-} from 'react-accessible-accordion';
+import Collapsible from 'react-collapsible';
 import "./Resources.css"
 
 export default class Resources extends React.Component {
     render() {
         return (
-            <Accordion>
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            COVID INFORMATION
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                        <p>
-                        Exercitation in fugiat est ut ad ea cupidatat ut in
-                        cupidatat occaecat ut occaecat consequat est minim minim
-                        esse tempor laborum consequat esse adipisicing eu
-                        reprehenderit enim.
-                        </p>
-                    </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            SUPPORT
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                        <p>
-                        In ad velit in ex nostrud dolore cupidatat consectetur
-                        ea in ut nostrud velit in irure cillum tempor laboris
-                        sed adipisicing eu esse duis nulla non.
-                        </p>
-                    </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            SUPPORT
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel>
-                        <p>
-                        In ad velit in ex nostrud dolore cupidatat consectetur
-                        ea in ut nostrud velit in irure cillum tempor laboris
-                        sed adipisicing eu esse duis nulla non.
-                        </p>
-                    </AccordionItemPanel>
-                </AccordionItem>
-               </Accordion>          
+            <div className="resources">
+                <h2>The community is SUPPORTIVE</h2>
+                <Collapsible trigger="COVID INFORMATION">
+                  <p>
+                    COVID News around the world
+                  </p>
+                  <p>
+                    COVID Map here.
+                  </p>
+                </Collapsible>
+                <Collapsible trigger="SUPPORT">
+                  <p>
+                    Mental support
+                  </p>
+                </Collapsible>
+                <Collapsible trigger="EMERGENCY REPORT">
+                  <p>
+                    If you encounter emergency situation, please post and notify people around.
+                  </p>
+                </Collapsible>
+            </div>        
         );
-      }
-}
+ }
