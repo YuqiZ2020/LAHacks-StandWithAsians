3c3a97caa7bd01946c055ccf863378d6199439f4
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-03-24 16:07:25 +0800

    Update Resources.js

--- frontend/src/Pages/resources/Resources.js
+++ frontend/src/Pages/resources/Resources.js
@@ -8,10 +8,22 @@ export default class Resources extends React.Component {
             <div className="resources">
                 <h2>The community is SUPPORTIVE</h2>
                 <Collapsible trigger="COVID INFORMATION">
+                  <p>
+                    COVID News around the world
+                  </p>
+                  <p>
+                    COVID Map here.
+                  </p>
                 </Collapsible>
                 <Collapsible trigger="SUPPORT">
+                  <p>
+                    Mental support
+                  </p>
                 </Collapsible>
                 <Collapsible trigger="EMERGENCY REPORT">
+                  <p>
+                    If you encounter emergency situation, please post and notify people around.
+                  </p>
                 </Collapsible>
             </div>        
         )
