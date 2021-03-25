44191aaac9a7f63999ab41195419a823f638034a
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-03-25 10:46:50 +0800

    Update Resources.js

--- frontend/src/Pages/resources/Resources.js
+++ frontend/src/Pages/resources/Resources.js
@@ -13,10 +13,10 @@ export default class Resources extends React.Component {
         <Collapsible trigger="+ COVID INFORMATION" triggerWhenOpen="- COVID INFORMATION">
           <p>
             COVID News around the world
-                  </p>
+          </p>
           <p>
             COVID Map here.
-                  </p>
+          </p>
           <iframe className="map" width="650" height="400" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" title="2019-nCoV" src="//arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c&extent=77.3846,11.535,163.5174,52.8632&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" ></iframe>
           <br />
           <span><small>Source:arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c</small></span>
@@ -24,12 +24,12 @@ export default class Resources extends React.Component {
         <Collapsible trigger="+ SUPPORT" triggerWhenOpen="- SUPPORT">
           <p>
             Mental support
-                  </p>
+          </p>
         </Collapsible>
         <Collapsible trigger="+ EMERGENCY REPORT" triggerWhenOpen="- EMERGENCY REPORT">
           <p>
             If you encounter emergency situation, please post and notify people around.
-                  </p>
+          </p>
         </Collapsible>
       </div>
     );
