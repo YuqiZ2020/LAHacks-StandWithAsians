c0921a96cd43688aeb6b7a99b6908854cb310821
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-03-25 16:26:09 +0800

    Update Resources.js

--- frontend/src/Pages/resources/Resources.js
+++ frontend/src/Pages/resources/Resources.js
@@ -38,32 +38,7 @@ export default class Resources extends React.Component {
         <h2>The community is SUPPORTIVE</h2>
         <Collapsible trigger="+ EVENTS" triggerWhenOpen="- EVENTS">
           <p>update protests and events here.</p>
-          <table className="eventable">
-            <tr>
-              <th>DATE</th>
-              <th>TIME</th>
-              <th>STATE</th>
-              <th>CITY</th>
-              <th>LOCATION</th>
-              <th>MAP</th>
-            </tr>
-            <tr>
-              <td>SAT 03/27/2021</td>
-              <td>11AM</td>
-              <td>CA</td>
-              <td>San Francisco</td>
-              <td>Saratoga City Hall</td>
-              <td><a href="https://www.google.com/maps/search/Saratoga+City+Hall/@34.3188346,-95.7084137,4z/data=!3m1!4b1">location</a></td>
-            </tr>
-            <tr>
-              <td>SAT 03/27/2021</td>
-              <td>12pM</td>
-              <td>CA</td>
-              <td>Los Angeles</td>
-              <td>200 N Spring Street</td>
-              <td><a href="https://www.google.com/maps/place/Los+Angeles+City+Hall,+200+N+Spring+St,+Los+Angeles,+CA+90012/@34.0535267,-118.2451203,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2c64f395f4df1:0xa210ca08f4b2deda!8m2!3d34.0535267!4d-118.2429316">location</a></td>
-            </tr>          
-          </table>  
+          <div>{events}</div>  
         </Collapsible>
         <Collapsible trigger="+ COVID INFORMATION" triggerWhenOpen="- COVID INFORMATION">
           <p>
