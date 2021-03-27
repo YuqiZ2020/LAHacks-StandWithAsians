43b0c8d16ff45c4596e237664339c98884023628
Yunqiu Han <yunqiu21@g.ucla.edu>
2021-03-27 22:06:00 +0800

    put about and home together

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Home.css";
+import NewsLetter from "../Components/newsLetter/newsLetter"
 import sample1 from "../Image/sample1.png";
 import sample2 from "../Image/sample2.png";
 import sample3 from "../Image/sample3.png";
@@ -8,15 +9,28 @@ import sample5 from "../Image/sample5.png";
 export default class Home extends React.Component {
     render() {
         return (
-            <div className="display-window">
-                <div className="image-list">
-                    <img src={sample1} alt="sample1"></img>
-                    <img src={sample2} alt="sample2"></img>
-                    <img src={sample3} alt="sample3"></img>
-                    <img src={sample4} alt="sample4"></img>
-                    <img src={sample5} alt="sample5"></img>
+            <div>
+                <div className="display-window">
+                    <div className="image-list">
+                        <img src={sample1} alt="sample1"></img>
+                        <img src={sample2} alt="sample2"></img>
+                        <img src={sample3} alt="sample3"></img>
+                        <img src={sample4} alt="sample4"></img>
+                        <img src={sample5} alt="sample5"></img>
+                    </div>
                 </div>
-            </div>
+                <div className='about-wrap'>
+
+                    <div className="about">
+                        <h1>Stop Asian Hate</h1>
+                        <p>This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. </p>
+
+                    </div>
+
+                </div>
+                <NewsLetter />
+            </div >
+            
         )
     }
 }
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -6,7 +6,6 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import About from "./About";
 import Forum from "../Pages/forum/Forum";
 import ForumDiscussion from "../Pages/forum/ForumDiscussion"
 import Gallery from "./Gallery";
@@ -22,20 +21,16 @@ export default class Navbar extends React.Component {
                 <div className="navbar">
                     <nav className="link-container">
                         <Link to="/" className="link">Home</Link>
-                        <Link to="/about" className="link">About</Link>
-
-
                         <div className='link'>
-                        <div className='dd'>
-                            <Link to="/news" className="link">News</Link>
-                            <div class="dropdown-content">
-                                <Link to="/news#news-news">News</Link>
-                                <Link to="/news#news-tweets">Tweets</Link>
-                                <Link to="/news#news-petitions">Petitions</Link>
-                                <a href="#news-data">Data</a>
-                                
+                            <div className='dd'>
+                                <Link to="/news" className="link">News</Link>
+                                <div class="dropdown-content">
+                                    <Link to="/news#news-news">News</Link>
+                                    <Link to="/news#news-tweets">Tweets</Link>
+                                    <Link to="/news#news-petitions">Petitions</Link>
+                                    <a href="#news-data">Data</a>
+                                </div>
                             </div>
-                        </div>
                         </div>
                         <Link to="/forum" className="link">Forum</Link>
                         <Link to="/gallery" className="link">Gallery</Link>
@@ -43,10 +38,9 @@ export default class Navbar extends React.Component {
                     </nav>
                     <Switch>
                         <Route path="/" exact component={Home} />
-                        <Route path="/about" component={About} />
                         <Route path="/news" component={News} />
-                        <Route exact path="/forum" component={Forum}/>
-                        <Route exact path="/forum/:postId" component={ForumDiscussion}/>
+                        <Route exact path="/forum" component={Forum} />
+                        <Route exact path="/forum/:postId" component={ForumDiscussion} />
                         <Route path="/gallery" component={Gallery} />
                         <Route path="/resources" component={Resources} />
                         <Route path="/review" component={Review} />
