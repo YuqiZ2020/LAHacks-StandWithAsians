903596313b24dc25f8decf80ebb5505557d8a310
Yunqiu Han <yunqiu21@g.ucla.edu>
2021-03-27 22:22:21 +0800

    disqus style

--- frontend/src/Pages/forum/ForumDiscussion.js
+++ frontend/src/Pages/forum/ForumDiscussion.js
@@ -1,42 +1,43 @@
-import React, {useState, useEffect} from 'react'
-import {useParams} from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom"
 import Disqus from "disqus-react"
 import ForumCard from "../../Components/forum/forumCard"
-import {db} from "../../firebase"
+import { db } from "../../firebase"
+import "./Forum.css"
 
-export default function ForumDiscussion(){
+export default function ForumDiscussion() {
     const [data, setData] = useState({})
     const dataId = useParams().postId
     const disqusShortname = "stop-asian-hate"
     const disqusConfig = {
-      url: "https://stop-asian-hate.disqus.com/"+dataId,
-      identifier: useParams().postId,
-      title: "Title of Your Article"
+        url: "https://stop-asian-hate.disqus.com/" + dataId,
+        identifier: useParams().postId,
+        title: "Title of Your Article"
     }
     console.log(useParams().postId)
 
     useEffect(() => {
         const docRef = db.collection("forum").doc(dataId)
-        docRef.get().then((doc) =>{
+        docRef.get().then((doc) => {
             console.log(doc.data())
             setData(doc.data())
         })
     }, []);
-    
 
 
 
 
-    return(
+
+    return (
         <div className="forum">
-        <ForumCard data={data} key={data.realId} />
+            <ForumCard data={data} key={data.realId} />
 
 
-        <Disqus.DiscussionEmbed
-            shortname={disqusShortname}
-            config={disqusConfig}
-            showMedia={true}
-            showParentComment={true}
+            <Disqus.DiscussionEmbed
+                shortname={disqusShortname}
+                config={disqusConfig}
+                showMedia={true}
+                showParentComment={true}
             />
         </div>
     )
